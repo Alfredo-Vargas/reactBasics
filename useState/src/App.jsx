8f85edf788581d2ce05b9087145d4e8efdae759b
Your Name <you@example.com>
2023-01-27 19:50:58 +0100

    use State implemented

---
+++ useState/src/App.jsx
@@ -0,0 +1,35 @@
+import { React, useState } from 'react'
+
+// classes do not implement hook, they achieve the same thing differently
+
+function App() {
+  const [count, setState] = useState(4)
+  const [theme, setTheme] = useState('blue')
+  // const count = state.count
+  // const theme = state.theme
+
+  // const theme = state.theme
+  // Gets called only once whenever we update the component
+  // const [count, setCount] = useState(() => {
+  //   console.log('run function')
+  //   return 4
+  // })
+  function decrementCount() {
+    setState(prevCount => prevCount - 1)
+  }
+
+  function incrementCount() {
+    setState(prevCount => prevCount + 1)
+    setTheme(prevTheme => 'red')
+  }
+  return (
+    <>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <span>{theme}</span>
+      <button onClick={incrementCount}>+</button>
+    </>
+  )
+}
+
+export default App
